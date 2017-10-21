fedd1252ad229995fc9162e43e79ebe839b49e8d
Maysaraodeh <maysaraodeh@MacBook-Pro.local>
2017-10-21 21:31:02 +0300

    solution

--- index.js
+++ index.js
@@ -1,28 +1,58 @@
-// step 2 git your input references
-
-//var worker_email = document.getElementById("worker_email");
-//var worker_email_delete = document.getElementById("worker_email_delete");
-//var worker_name = document.getElementById("worker_name");
-//var worker_age = document.getElementById("worker_age");
-//var customer_email = document.getElementById("cust_email");
-//var customer_name = document.getElementById("cust_name");
-//var customer_age = document.getElementById("cust_age");
-//var add_customer_btn = document.getElementById("add_customer");
-//var add_worker_btn = document.getElementById("add_worker");
-
-// step 3 Adding customer data to firebase
-// step 4 Adding workers data to firebase
-// step 5 retrive customers from the firebase 
-// step 6 retrive workers from firebase
-//step 7 delete worker using the email
-// step 8 delete customer using the email
-
-//step 9 to be continued in the next tutorials
+// here we are getting references from fields that we are going to work with.
+// check the id for the fields in <input> tag int the index.html
+var worker_email = document.getElementById("worker_email");
+var worker_email_delete = document.getElementById("worker_email_delete");
+var worker_name = document.getElementById("worker_name");
+var worker_age = document.getElementById("worker_age");
+var customer_email = document.getElementById("cust_email");
+var customer_name = document.getElementById("cust_name");
+var customer_age = document.getElementById("cust_age");
+var add_customer_btn = document.getElementById("add_customer");
+var add_worker_btn = document.getElementById("add_worker");
 
 
 
+//create a database reference to the node workers.
+var workerRootRef = firebase.database().ref().child("workers");
+// here we are retriving the data from the database
+workerRootRef.on('child_added' , snap=>{
+  var name = snap.child("name").val();
+  var email = snap.child("email").val();
+  var age = snap.child("age").val();
+  $("#table_workers").append("<tr><td>"+email+"</td><td>"+name+"</td><td>"+age+"</td></tr>");
 
-// add customers dialog
+})
+
+// the same thing as we did in the workers.
+var customerRootRef = firebase.database().ref().child("customers");
+customerRootRef.on('child_added' , snap=>{
+  var name = snap.child("name").val();
+  var email = snap.child("email").val();
+  var age = snap.child("age").val();
+
+  $("#table_customers").append("<tr><td>"+email+"</td><td>"+name+"</td><td>"+age+"</td></tr>");
+
+})
+
+//this function push the worker's data that we get from the form to the database.
+function add_worker_db(){
+  worker_data = {name : worker_name.value , email : worker_email.value , age : worker_age.value};
+
+  var firebaseRef = firebase.database().ref().child("workers");
+  firebaseRef.push(worker_data);
+
+}
+
+// the same as we did for the workers.
+function add_customer_db(){
+  customer_data = {name : customer_name.value , email : customer_email.value , age : customer_age.value};
+  var firebaseRef = firebase.database().ref().child("customers");
+  // push data to dtabase with an auto generated id.
+  firebaseRef.push(customer_data);
+
+}
+
+// this function triggerd when you click the (plus button above the customer's table) and show the form of adding a customer.
 function add_customer_dialog(){
   var dialog = document.getElementById('customer');
   var closeDialog = document.querySelector('#add_customer');
@@ -37,9 +67,8 @@ function add_customer_dialog(){
     dialog.close();
   });
 }
+// this function triggerd when you click the (plus button above the worker's table) and show the form of adding a worker.
 
-
-//add workers dialog
 function add_worker_dialog(){
 
   var showDialogButton = document.querySelector('#show-dialog-worker');
@@ -58,7 +87,8 @@ function add_worker_dialog(){
 
 }
 
-// delete workers dialog
+// this function triggerd when you click the (delete button above the worker's table) and show the form of adding a worker.
+
 function delete_worker_dialog(){
   var showDialogButton = document.querySelector('#show-dialog-delete-worker');
   var closeDialog = document.querySelector('#delete_worker');
@@ -76,3 +106,17 @@ function delete_worker_dialog(){
 
 }
 
+// this function deletes the worker's record from the database with the email we provided.
+function delete_worker_db(){
+  var firebaseRefDelete = firebase.database().ref().child("workers");
+
+  var query = firebaseRefDelete.orderByChild('email').equalTo(worker_email_delete.value);
+  query.on('child_added', function(snapshot) {
+      snapshot.ref.remove();
+  })
+
+}
+
+// now do the customer record deletion and send the code to my facebook account to check it and give you help with it.
+
+// thank you all.
